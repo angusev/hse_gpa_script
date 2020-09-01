617db0787e4f0386deba07ad526b65df8823af3b
Andrey <aagusev_2@edu.hse.ru>
2020-09-01 16:03:41 +0300

    refactor

--- gpa.py
+++ gpa.py
@@ -8,46 +8,50 @@ WORD2MARK = {'отлично': 5,
 }
 
 
-def get_gpa(soup, scale=10):
+def get_gpas(soup):
     table = soup.find('tbody')
 
     table_elems = list(table.children)
 
-    gpa = 0.
+    gpa5 = 0.
+    gpa10 = 0.
     total_credits = 0
 
+    print('SUBJECT'.ljust(70), '', '', 'CREDITS', sep='\t')
     for i in range(len(table_elems)):
         if table_elems[i].name == 'tr':
             subject_tags = list(table_elems[i].children)
-            for j in range(len(subject_tags)):
+            for j in range(2, len(subject_tags)):
+                str_mark5 = subject_tags[j].string
+                str_mark10 = subject_tags[j-2].string
 
-                if subject_tags[j].string in WORD2MARK.keys() and subject_tags[j-2].string:
-                    if scale == 10:
-                        mark = int(subject_tags[j-2].string)
-                    elif scale == 5:
-                        mark = WORD2MARK[subject_tags[j].string]
-                    else:
-                        raise ValueError('Unknown scale')
+                if str_mark5 in WORD2MARK.keys() and str_mark10:
+                    mark10 = int(str_mark10)
+                    mark5 = WORD2MARK[str_mark5]
 
                     credits = int(subject_tags[j-3].string)
-                    gpa += mark * credits
+                    gpa5 += mark5 * credits
+                    gpa10 += mark10 * credits
                     total_credits += credits
 
-    gpa /= total_credits
-    return gpa
+                    print(subject_tags[j-11].string.ljust(70, '_'), str_mark5, str_mark10, credits, sep='\t')
+
+    print('TOTAL CREDITS:'.ljust(70), '', '', total_credits, sep='\t')
+    print()
+    gpa5 /= total_credits
+    gpa10 /= total_credits
+    return gpa5, gpa10
 
 
 if __name__ == '__main__':
-
     parser = argparse.ArgumentParser()
     parser.add_argument('--filename', '-f', type=str, default='LMS HSE.html')
-    parser.add_argument('--scale', type=float, default=5.)
     args = parser.parse_args()
 
     with open(args.filename, 'r') as f:
         html_doc = f.read()
     soup = BeautifulSoup(html_doc, 'html.parser')
 
-    gpa = get_gpa(soup, args.scale)
+    gpas = get_gpas(soup)
 
-    print(f'GPA: {round(gpa, 2)}/{args.scale}')
+    print(f'GPA: {round(gpas[0], 2)}/5.0, {round(gpas[1], 2)}/10.0')
